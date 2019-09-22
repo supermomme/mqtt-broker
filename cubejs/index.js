fe2bdd7e8c89037c0f50a669a3874d71c7555a02
Momme Jürgensen <momme@juergensen.me>
2019-09-22 19:24:35 +0200

    remove cubejs folder

--- cubejs/index.js
+++
@@ -1,7 +0,0 @@
-const CubejsServer = require('@cubejs-backend/server');
-
-const server = new CubejsServer({ devServer: true });
-
-server.listen().then(({ port }) => {
-  console.log(`🚀 Cube.js server is listening on ${port}`);
-});
