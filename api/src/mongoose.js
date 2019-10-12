1dfb752ce500f78ef66c234a1478bc139cbdd7ed
Momme Jürgensen <momme@juergensen.me>
2019-10-12 17:09:26 +0200

    fix env

--- api/src/mongoose.js
+++ api/src/mongoose.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 
 module.exports = function (app) {
+  const mongoConfig = app.get('mongo')
+  let user = (mongoConfig.username && mongoConfig.password) ? `${mongoConfig.username}:${mongoConfig.password}@` : ''
+
   mongoose.connect(
-    app.get('mongodb'),
+    `mongodb://${user}${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.db}`,
     { useCreateIndex: true, useNewUrlParser: true }
   )
   mongoose.Promise = global.Promise
