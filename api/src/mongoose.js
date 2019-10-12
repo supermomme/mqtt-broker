80c7416e31e01530c6a7818ac9de3a171eb9824c
Momme Jürgensen <momme@juergensen.me>
2019-10-12 18:33:21 +0200

    fix

--- api/src/mongoose.js
+++ api/src/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 module.exports = function (app) {
   const mongoConfig = app.get('mongo')
   let user = (mongoConfig.username && mongoConfig.password) ? `${mongoConfig.username}:${mongoConfig.password}@` : ''
-  let mongoUrl = `mongodb://${user}${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.db}`
+  let mongoUrl = `mongodb://${user}${mongoConfig.host}:${mongoConfig.port}/${mongoConfig.db}?authSource=admin`
   console.log(mongoUrl)
   mongoose.connect(
     mongoUrl,
